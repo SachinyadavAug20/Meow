5814bc678fca0abc7e490fcbde4ca2bc4048a6e4
SachinYadavAug20 <samtagon777@gmail.com>
2026-06-06 12:40:07 +0530

    Home screen UI

--- packages/cli/src/index.tsx
+++ packages/cli/src/index.tsx
@@ -2,13 +2,14 @@ import { createCliRenderer } from "@opentui/core";
 import { createRoot } from "@opentui/react";
 import { createMemoryRouter, RouterProvider } from "react-router";
 import { RootLayout } from "../layouts/root-layout";
+import { Home } from "./screens/home";
 
 const router=createMemoryRouter([
   {
     path:'/',
     element:<RootLayout/>,
     children:[
-      {index:true,element:<box><text>Home</text></box>},
+      {index:true,element:<Home/>},
       {path:'session/new',element:<box><text>session/new</text></box>},
       {path:'session/:id',element:<box><text>session/:id</text></box>},
     ]
